65ed85377b2fb5565feef048fb0b33d201053c07
HkingAuditore <178854663@qq.com>
2020-06-13 18:30:01 +0800

    修复bug/支持特殊符号/大小写保留/输入验证

--- Scripts/MonoalphabeticSubstitutionCipher.js
+++ Scripts/MonoalphabeticSubstitutionCipher.js
@@ -2,8 +2,13 @@
 
 // 生成密码本
 function generateKeyArray(key) {
-    var keyArray = new String(key).toUpperCase().split("");
+    var keyArray = new String(key).split("");
     for (let i = 'A'.charCodeAt(); i <= 'Z'.charCodeAt(); i++) {
+        if (jQuery.inArray(String.fromCharCode(i), keyArray) == -1) {
+            keyArray.push(String.fromCharCode(i));
+        }
+    }
+    for (let i = 'a'.charCodeAt(); i <= 'z'.charCodeAt(); i++) {
         if (jQuery.inArray(String.fromCharCode(i), keyArray) == -1) {
             keyArray.push(String.fromCharCode(i));
         }
@@ -16,7 +21,7 @@ function generateKeyArray(key) {
 
 // 加密
 function MonoEncode(source, key) {
-    var sourceString = new String(source).toUpperCase();
+    var sourceString = new String(source);
 
     // 字符串转数组
     var result = new Array();
@@ -27,15 +32,20 @@ function MonoEncode(source, key) {
 
     // 遍历字符
     sourceArray.forEach((value, index, array) => {
+        if (!/[A-Za-z]+/.test(value)) {
+            result.push(value);
+        } else {
+            offset = value.toUpperCase() === value ? 0 : 6;
+            result.push(keyArray[value.charCodeAt() - 'A'.charCodeAt() - offset]);
+        }
         // 生成密码串
-        result.push(keyArray[value.charCodeAt() - 'A'.charCodeAt()]);
     })
     return result.join("");
 }
 
 // 解密
 function MonoDecode(source, key) {
-    var sourceString = new String(source).toUpperCase();
+    var sourceString = new String(source);
 
     // 字符串转数组
     var result = new Array();
@@ -46,10 +56,16 @@ function MonoDecode(source, key) {
 
     // 遍历字符
     sourceArray.forEach((value, index, array) => {
-        // 生成解密串
-        targetChar = jQuery.inArray(value, keyArray);
-        if (result != -1)
-            result.push(String.fromCharCode(targetChar + 'A'.charCodeAt()));
+        if (!/[A-Za-z]+/.test(value)) {
+            result.push(value);
+        } else {
+            // 生成解密串
+            targetChar = jQuery.inArray(value, keyArray);
+            if (result != -1) {
+                offset = targetChar > 25 ? 6 : 0;
+                result.push(String.fromCharCode(targetChar + 'A'.charCodeAt() + offset));
+            }
+        }
     })
     return result.join("");
 }
--- Scripts/VigenereCipher.js
+++ Scripts/VigenereCipher.js
@@ -4,22 +4,28 @@
 
 // 加密
 function VigenereEncode(source, key) {
-    var sourceString = new String(source).toUpperCase();
-    var keyString = new String(key).toUpperCase();
+    var sourceString = String(source);
+    var keyString = String(key).toUpperCase();
     // 字符串转数组
     var result = new Array();
     var sourceArray = sourceString.split("");
     sourceArray.forEach(function(value, index, array) {
-        var curValue = value.charCodeAt();
-        var curKeyValue = key[index % keyString.length].charCodeAt();
-        result.push(GetEncodeTrgetChar(curValue, curKeyValue));
+        if (!/[A-Za-z]+/.test(value)) {
+            result.push(value);
+        } else {
+            var curValue = value.charCodeAt();
+            var curKeyValue = keyString[index % keyString.length].charCodeAt();
+            result.push(GetEncodeTrgetChar(curValue, curKeyValue));
+        }
     })
     return result.join("");
 }
 
 // 输入两个字符序列号，输出加密目标字符
 function GetEncodeTrgetChar(source, key) {
-    return String.fromCharCode('A'.charCodeAt() + (source + key) % 26);
+    indicator = source >= 'a'.charCodeAt() ? 'a'.charCodeAt() : 'A'.charCodeAt();
+    key = source >= 'a'.charCodeAt() ? key + 32 : key;
+    return String.fromCharCode(indicator + (source + key - 2 * indicator) % 26);
 }
 
 /////////////////////////////////////////////////////////
@@ -28,23 +34,29 @@ function GetEncodeTrgetChar(source, key) {
 
 // 解密
 function VigenereDecode(source, key) {
-    var sourceString = new String(source).toUpperCase();
-    var keyString = new String(key).toUpperCase();
+    var sourceString = String(source);
+    var keyString = String(key).toUpperCase();
     // 字符串转数组
     var result = new Array();
     var sourceArray = sourceString.split("");
     sourceArray.forEach(function(value, index, array) {
-        var curValue = value.charCodeAt();
-        var curKeyValue = key[index % keyString.length].charCodeAt();
-        result.push(GetDecodeTrgetChar(curValue, curKeyValue));
+        if (!/[A-Za-z]+/.test(value)) {
+            result.push(value);
+        } else {
+            var curValue = value.charCodeAt();
+            var curKeyValue = keyString[index % keyString.length].charCodeAt();
+            result.push(GetDecodeTrgetChar(curValue, curKeyValue));
+        }
     })
     return result.join("");
 }
 
 // 输入两个字符序列号，输出解密目标字符
 function GetDecodeTrgetChar(source, key) {
-    var partition = source < key ? 1 : 0;
-    return String.fromCharCode('A'.charCodeAt() + 26 * partition + source - key);
+    indicator = source >= 'a'.charCodeAt() ? 'a'.charCodeAt() : 'A'.charCodeAt();
+    key = source >= 'a'.charCodeAt() ? key + 32 : key;
+    partition = source < key ? 1 : 0;
+    return String.fromCharCode(indicator + 26 * partition + source - key);
 }
 
 /////////////////////////////////////////////////////////
